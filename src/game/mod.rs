c15b90ad45a585108e0c7c650340473361f24957
Jack Vickeridge <jackv@gmail.com>
2024-06-09 13:50:58 -0400

    Move Render to render submod.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,5 +1,3 @@
-use macroquad::prelude::*;
-
 // FIXME: Work out which types should be exported and remove use ::*.
 // STUB: Move to separate modules not submodules with visibility.
 mod play;
@@ -7,6 +5,10 @@ use play::*;
 
 mod input;
 use input::*;
+
+mod render;
+use render::RenderLev;
+use render::RenderSplash;
 
 mod map;
 use map::*;
@@ -102,118 +104,9 @@ impl Game {
 }
 
 
-// PLAY
-//
 // FIXME: Move Play to play submod.
-
 // Whether we are currently playing a level, in intro screen, in game over, etc
 enum Mode {
     Splash,
     LevPlay,
-}
-
-//
-// RENDER
-//
-// FIXME: Move Render structs to render submod?
-//
-
-// Render state for one frame of level
-// Currently not needing any global graphics state
-struct RenderLev {
-    // COORDS FOR CURRENT FRAME. In gl units which are pixels.
-    // Distance from edge of drawing surface to play area
-    offset_x: f32,
-    // Distance from edge of drawing surface to play area
-    offset_y: f32,
-    // Size of each tile
-    sq_w: f32,
-    sq_h: f32,
-}
-
-impl RenderLev {
-    fn begin(w: u16, h: u16) -> RenderLev {
-        assert_eq!(w, h);
-        let game_size = screen_width().min(screen_height());
-        let offset_y = (screen_height() - game_size) / 2. + 10.;
-
-        let r = RenderLev {
-            // FIXME: Why does this work with landscape orientation?
-            offset_x: (screen_width() - game_size) / 2. + 10.,
-            offset_y: (screen_height() - game_size) / 2. + 10.,
-            sq_w: (screen_height() - offset_y * 2.) / w as f32,
-            sq_h: (screen_height() - offset_y * 2.) / w as f32,
-        };
-
-        r._draw_backdrop();
-
-        r
-    }
-
-    fn _draw_backdrop(&self)
-    {
-        clear_background(LIGHTGRAY);
-
-        draw_text(format!("Level: 1", ).as_str(), 10., 20., 20., DARKGRAY);
-    }
-
-    // Draw ent's texture/colour to the screen at specified tile coords.
-    // Works out pixel coords given pixel size of play area in RenderLev.
-    fn draw_ent(
-        self: &RenderLev,
-        // View coords in map. Relative to first visible tile (currently always the same).
-        vx: i16,
-        vy: i16,
-        // Ent to draw
-        ent: &Ent,
-    ) {
-       let px = self.offset_x + self.sq_w * vx as f32;
-       let py = self.offset_y + self.sq_h * vy as f32;
-
-        if let Some(col) = ent.fill {
-            draw_rectangle(px, py, self.sq_w, self.sq_h, col);
-        }
-
-        if let Some(col) = ent.border {
-            draw_rectangle_lines(px, py, self.sq_w, self.sq_h, 2., col);
-        }
-
-        if let Some(tex) = &ent.tex {
-            draw_texture_ex(
-                &tex,
-                px,
-                py,
-                WHITE,
-                DrawTextureParams {
-                    dest_size: Some(vec2(self.sq_w, self.sq_h)),
-                    ..Default::default()
-                },
-            );
-        }
-    }
-}
-
-// Render state for one frame of "Show text, press enter to continue"
-// Currently not needing any global graphics state
-struct RenderSplash {
-}
-
-impl RenderSplash
-{
-    fn begin(text: &str) -> RenderSplash {
-        clear_background(WHITE);
-        let font_size = 30.;
-        let text_size = measure_text(text, None, font_size as _, 1.0);
-
-        // FIXME: Multi-line text. Ideally with dialog pics etc.
-        draw_text(
-            text,
-            screen_width() / 2. - text_size.width / 2.,
-            screen_height() / 2. + text_size.height / 2.,
-            font_size,
-            DARKGRAY,
-        );
-
-        RenderSplash {}
-    }
 }
