439b89d57c271619d05419d8e3f06d4b08d19eb4
Jack Vickeridge <jackv@cantab.net>
2024-09-22 13:40:35 +0100

    Remove macroquad from simple tests.
    Will want some "run these same tests but with rendering" test.

--- src/test.rs
+++ src/test.rs
@@ -49,9 +49,8 @@ mod basic_tests {
         )
     }
 
-    // To get tests to run without macroquad, need to omit all texture functions, even Texture2D::empty()
-    #[macroquad::test]
-    async fn basic_move() {
+    #[test]
+    fn basic_move() {
         let p = get_lev(1);
     }
 }
