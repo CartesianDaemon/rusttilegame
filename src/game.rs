032131b6b86c2c7cda478fd3581178b33fd412c3
Jack Vickeridge <jackv@cantab.net>
2024-10-10 21:05:21 +0100

    Smaller pause between ghosts.

--- src/game.rs
+++ src/game.rs
@@ -14,9 +14,10 @@ pub struct Game<Levs: levset::LevSet> {
     pub lev_set: Levs, // TODO
     play_state: Play,
     ghost_state: Play,
-    n_ghost_ticks: u32, // Move into play state?
-    preghost_ticks: u32,
-    max_ghost_ticks: u32,
+    n_ghost_ticks: i32, // Move into play state?
+    preghost_ticks: i32,
+    max_ghost_ticks: i32,
+    tween_ghost_ticks: i32,
     input: Input,
 }
 
@@ -31,12 +32,13 @@ impl<Levs: levset::LevSet> Game<Levs> {
             n_ghost_ticks: 0,
             preghost_ticks: 3,
             max_ghost_ticks: 5,
+            tween_ghost_ticks: 1,
         }
     }
 
-    fn reset_ghost_state(&mut self) {
+    fn reset_ghost_state(&mut self, ticks_before_visibility: i32) {
         self.ghost_state = self.play_state.clone();
-        self.n_ghost_ticks = 0;
+        self.n_ghost_ticks = self.preghost_ticks - ticks_before_visibility;
     }
 
     fn ghost_pc(&self) -> f32 {
@@ -57,11 +59,11 @@ impl<Levs: levset::LevSet> Game<Levs> {
             if let Some(to_lev) = maybe_to_lev {
                 self.play_state = self.lev_set.load_lev_stage(&to_lev);
             }
-            self.reset_ghost_state();
+            self.reset_ghost_state(self.preghost_ticks);
         } else if self.input.ready_to_advance_ghost_state() {
             self.n_ghost_ticks += 1;
             if self.n_ghost_ticks >= self.preghost_ticks + self.max_ghost_ticks {
-                self.reset_ghost_state();
+                self.reset_ghost_state(self.tween_ghost_ticks);
             } else if self.n_ghost_ticks >= self.preghost_ticks {
                 self.ghost_state.advance(&mut self.input);
             }
