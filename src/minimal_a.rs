0f42708980327d9b03cada3e1d67e0054f5a56d0
Jack Vickeridge <jackv@cantab.net>
2025-02-15 19:39:55 +0000

    And separate move function.

--- src/minimal_a.rs
+++ src/minimal_a.rs
@@ -14,6 +14,11 @@ impl Map
         Map {loc_objs: vec![loc_dancer_on_floor(), loc_empty_floor(), loc_empty_floor(), loc_empty_floor()]}
     }
 
+    pub fn move_to(&mut self, handle: (usize, usize), new_pos: usize) {
+        let obj = self.loc_objs[handle.0].remove(handle.1);
+        self.loc_objs[new_pos+1].push(obj);
+    }
+
     pub fn iterate(&mut self) {
         for idx in 0..self.loc_objs.len() {
             let loc = &self.loc_objs[idx];
@@ -22,9 +27,7 @@ impl Map
                 Some(obj) => obj
             };
             if obj.auto_move_right && idx +1 < self.loc_objs.len() {
-                // TODO: Move into movement function
-                let obj = self.loc_objs[idx].remove(1);
-                self.loc_objs[idx+1].push(obj);
+                self.move_to( (idx,1), idx+1 );
             }
         }
     }
