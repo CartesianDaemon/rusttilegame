05f395e4a94ce6c38e6d032d39cb4e4b6879ca90
Jack Vickeridge <jackv@cantab.net>
2025-11-01 20:44:00 +0000

    WIP: Add scripts for programming puzzle. Currently unused.

---
+++ src/programming_puzzle_scripts/movement_logic.rs
@@ -0,0 +1,21 @@
+// TODO: Add these types to a Script struct?
+use crate::engine::scripting::*;
+use super::obj_types::*;
+
+pub fn passable(field: &Field, pos: MapCoord) -> bool {
+    field.all_pass(pos, Pass::Empty)
+}
+
+pub fn impassable(field: &Field, pos: MapCoord) -> bool {
+    !passable(field, pos)
+}
+
+pub fn move_mov(field: &mut Field, mov: RosterIndex, cmd: Cmd) -> SceneContinuation {
+    let hero = field.hero();
+    match field[mov].props.ai {
+        AI::Prog => {
+            // TODO
+        },
+    }
+    return SceneContinuation::Continue(());
+}
---
+++ src/programming_puzzle_scripts/obj_types.rs
@@ -0,0 +1,20 @@
+
+// STUB: Properties that can be applied to Ent.
+// Makes more sense if there's a mod with Ent class and non-public classes only used inside it.
+// Can these become a generic properties defined in load, rather than being specified by engine?
+
+// Passable. Whether other movs can move through an ent or not.
+#[derive(Clone, PartialEq, Debug)]
+pub enum Pass {
+    Empty, // No impediment to movement, e.g. floor.
+    Solid, // Block movement, e.g. wall.
+    Mov, // Something which can move itself, e.g. hero, enemy
+    // INSERT: Obj, // Something which can be moved or maybe coexisted with, e.g. furniture
+}
+
+// Types of movement-control logic ents can use
+#[derive(Copy, Clone, PartialEq, Debug)]
+#[allow(dead_code)]
+pub enum AI {
+    Prog, // Move according to instruction sequence: F, L, etc.
+}
