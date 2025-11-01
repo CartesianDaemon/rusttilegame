25322c9417bbc01fdc5b01c70c2cb58e77f06a72
Jack Vickeridge <jackv@cantab.net>
2025-11-01 13:19:13 +0000

    WIP: Update paths for wasm release.

--- build_scripts/release.sh
+++ build_scripts/release.sh
@@ -2,6 +2,6 @@
 
 # Run from source root
 cargo build -r --target wasm32-unknown-unknown
-cp target/wasm32-unknown-unknown/release/tilegame.wasm docs/tilegame_release.wasm
+cp target/wasm32-unknown-unknown/release/cartesian_tile_engine_demo.wasm docs/cartesian_tile_engine_demo.wasm
 # Freeze version of assets used. TODO: Use rsync to remove old assets from docs/imgs.
 cp imgs/* docs/imgs/
