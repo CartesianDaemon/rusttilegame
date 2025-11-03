10dab52d779def06c1dadf66376ae7c0c0e0ce90
Jack Vickeridge <jackv@cantab.net>
2025-11-03 22:29:01 +0000

    WIPWIP: Split Obj into Visual and Logical.

--- src/bin/engine/obj.rs
+++ src/bin/engine/obj.rs
@@ -1,36 +1,21 @@
 use super::map_coords::CoordDelta;
 use super::obj_scripting_properties;
 
-// TODO: Need to avoid using scripts except via Gamedata template
-//use crate::scripts::*;
-
 use macroquad::prelude::*;
 
-/// Anything tile-sized and drawable including floor, wall, object, being.
-/// Containing name, visuals, scripting properties, etc but not coords in map.
-/// Everything that would compare equal for two identical objects at different places.
+/// An Obj is anything tile-sized and drawable: floor, wall, object, being.
+/// Representing an object not placed in the map. May not be used.
 #[derive(Clone, Debug)]
-pub struct ObjProperties {
+pub struct FreeObj {
+    pub logical_props: LogicalProps,
+    pub visual_props: VisualProps,
+}
+
+/// Logical properties of object, used for game logic and scripting.
+#[derive(Clone, Debug, PartialEq)]
+pub struct LogicalProps {
     /// String representation of object, used internally for debug fmt etc.
     pub name: String,
-
-    // Visual display properties.
-    // Only used by Render. Worth moving into a separate struct shared between Map and Render?
-    // If we replace Texture with path and initialise texture only in Render, we should break
-    // dependency on macroquad runtime code.
-
-    pub border: Option<Color>,
-    pub fill: Option<Color>,
-
-    // For now, tex is animated all the time including stationary.
-    // TODO: Consider AnimState which specifies which ones should be.
-    pub tex_paths: Vec<String>,
-    pub tex_scale: f32,
-
-    pub text: Option<String>,
-    pub text_col: Option<Color>,
-
-    // Ent properties and behaviour, used by Game logic and scripting
 
     // Solidity, e.g. wall, floor
     pub pass: obj_scripting_properties::Pass,
@@ -45,17 +30,25 @@ pub struct ObjProperties {
     pub effect: obj_scripting_properties::Effect,
 }
 
-impl ObjProperties {
-    fn defaults() -> ObjProperties {
-        ObjProperties {
-            name: "????".to_string(),
+/// Visual display properties. Only used by Render.
+#[derive(Clone, Debug)]
+pub struct VisualProps {
+    pub border: Option<Color>,
+    pub fill: Option<Color>,
 
-            border: None,
-            fill: None,
-            tex_paths: vec![],
-            tex_scale: 1.0,
-            text: None,
-            text_col: None,
+    // For now, tex is animated all the time including stationary.
+    // TODO: Consider AnimState which specifies which ones should be.
+    pub tex_paths: Vec<String>,
+    pub tex_scale: f32,
+
+    pub text: Option<String>,
+    pub text_col: Option<Color>,
+}
+
+impl LogicalProps {
+    fn defaults() -> Self {
+        Self {
+            name: "????".to_string(),
 
             pass: obj_scripting_properties::Pass::Empty,
             ai: obj_scripting_properties::AI::Stay, // STUB: Could use this as a better placeholder flag
@@ -65,67 +58,8 @@ impl ObjProperties {
         }
     }
 
-    pub fn assets_path() -> String {
-        "imgs/".to_string()
-    }
-
-    #[allow(dead_code)]
-    pub fn new_tex(tex_path: &str) -> ObjProperties {
-        ObjProperties {
-            tex_paths: vec![Self::assets_path() + tex_path],
-            ..ObjProperties::defaults()
-        }
-    }
-
-    pub fn new_tex_col(tex_path: &str, fill: Color) -> ObjProperties {
-        ObjProperties {
-            tex_paths: vec![Self::assets_path() + tex_path],
-            fill: Some(fill),
-            ..ObjProperties::defaults()
-        }
-    }
-
-    /// TODO: Need to add "rotate" option for directional movs.
-    /// TODO: Fix path in wasm. No prefix?
-    /// TODO: Bigger fish?
-    pub fn new_tex_anim(tex_paths: Vec<&str>) -> ObjProperties {
-        ObjProperties {
-            // TODO: Consider using a list comprehension crate
-            // TODO: Consider implementing my abbreviated map chain crate.
-            //       Note whether that could usefully do .iter() and .collect()?
-            // TODO: Consider whether simpler for caller to offer wildcard like "FishB*.png"
-            // TODO: Consider where to specify path to imgs? Here? As part of levset?
-            tex_paths: tex_paths.iter().map(|x| Self::assets_path() + x).collect(),
-            ..ObjProperties::defaults()
-        }
-    }
-
-    pub fn new_col(fill: Color) -> ObjProperties {
-        ObjProperties {
-            fill: Some(fill),
-            ..ObjProperties::defaults()
-        }
-    }
-
-    pub fn new_col_outline(fill: Color, outline: Color) -> ObjProperties {
-        ObjProperties {
-            fill: Some(fill),
-            border: Some(outline),
-            ..ObjProperties::defaults()
-        }
-    }
-
-    pub fn new_text_fill(text: String, fill: Option<Color>, text_col: Option<Color>) -> ObjProperties {
-        ObjProperties {
-            text: Some(text),
-            fill,
-            text_col,
-            ..ObjProperties::defaults()
-        }
-    }
-
     // FUNCTIONS REFERRING TO SPECIFIC PROPERTIES
-    // STUB: Could be combined if properties are made more generic.
+    // NB: Could be combined if properties are made more generic.
 
     // Todo: Replace with more meaningful "is_hero" fn in scripts. Or obj_properties??
     pub fn is_hero(ai: obj_scripting_properties::AI) -> bool {
@@ -141,16 +75,76 @@ impl ObjProperties {
     pub fn is_any_mov(ai: obj_scripting_properties::AI) -> bool {
         ai != obj_scripting_properties::AI::Stay
     }
-
-    fn comparable_fields(&self) -> (&String, &CoordDelta, &obj_scripting_properties::AI, &obj_scripting_properties::Pass) {
-        (&self.name, &self.dir, &self.ai, &self.pass)
-    }
 }
 
-/// Somewhat fuzzy match used for determining ascii representation.
-/// Ideally would have a different name not PartialEq.
-impl PartialEq for ObjProperties {
-    fn eq(&self, other:&Self) -> bool {
-        self.comparable_fields() == other.comparable_fields()
+impl VisualProps {
+    fn defaults() -> Self {
+        Self {
+            border: None,
+            fill: None,
+            tex_paths: vec![],
+            tex_scale: 1.0,
+            text: None,
+            text_col: None,
+        }
+    }
+
+    pub fn assets_path() -> String {
+        "imgs/".to_string()
+    }
+
+    #[allow(dead_code)]
+    pub fn new_tex(tex_path: &str) -> Self {
+        Self {
+            tex_paths: vec![Self::assets_path() + tex_path],
+            ..Self::defaults()
+        }
+    }
+
+    pub fn new_tex_col(tex_path: &str, fill: Color) -> Self {
+        Self {
+            tex_paths: vec![Self::assets_path() + tex_path],
+            fill: Some(fill),
+            ..Self::defaults()
+        }
+    }
+
+    /// TODO: Need to add "rotate" option for directional movs.
+    /// TODO: Fix path in wasm. No prefix?
+    /// TODO: Bigger fish?
+    pub fn new_tex_anim(tex_paths: Vec<&str>) -> Self {
+        Self {
+            // TODO: Consider using a list comprehension crate
+            // TODO: Consider implementing my abbreviated map chain crate.
+            //       Note whether that could usefully do .iter() and .collect()?
+            // TODO: Consider whether simpler for caller to offer wildcard like "FishB*.png"
+            // TODO: Consider where to specify path to imgs? Here? As part of levset?
+            tex_paths: tex_paths.iter().map(|x| Self::assets_path() + x).collect(),
+            ..Self::defaults()
+        }
+    }
+
+    pub fn new_col(fill: Color) -> Self {
+        Self {
+            fill: Some(fill),
+            ..Self::defaults()
+        }
+    }
+
+    pub fn new_col_outline(fill: Color, outline: Color) -> Self {
+        Self {
+            fill: Some(fill),
+            border: Some(outline),
+            ..Self::defaults()
+        }
+    }
+
+    pub fn new_text_fill(text: String, fill: Option<Color>, text_col: Option<Color>) -> Self {
+        Self {
+            text: Some(text),
+            fill,
+            text_col,
+            ..Self::defaults()
+        }
     }
 }
