3d4ab3878e3c639d7a43a015cf81466fc3e9ea10
Jack Vickeridge <jackv@cantab.net>
2025-11-08 14:55:13 +0000

    And: Update lev types in prog puzz.

--- src/bin/progpuzz/levels.rs
+++ src/bin/progpuzz/levels.rs
@@ -6,11 +6,7 @@ use crate::engine::for_gamedata::*;
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum BiobotPaneId {
-    NewGame,
-    LevIntro(u16),
-    LevArena(u16),
-    LevOutro(u16),
-    LevRetry(u16),
+    LevSplit(u16),
     Win,
 }
 
@@ -21,27 +17,22 @@ pub struct ProgpuzzLevset {
 
 impl ProgpuzzLevset {
     pub fn new() -> ProgpuzzLevset {
-        ProgpuzzLevset { current_paneid: BiobotPaneId::NewGame }
+        ProgpuzzLevset { current_paneid: BiobotPaneId::LevSplit(1) }
     }
 
     pub fn advance_pane(&mut self, continuation: PaneConclusion) {
         self.current_paneid = match (self.current_paneid, continuation) {
-            (BiobotPaneId::NewGame, PaneConclusion::SplashNext) => BiobotPaneId::LevIntro(1),
-            (BiobotPaneId::LevIntro(levnum), PaneConclusion::SplashNext) => BiobotPaneId::LevArena(levnum),
-            (BiobotPaneId::LevArena(levnum), PaneConclusion::ArenaWin) => BiobotPaneId::LevOutro(levnum),
-            (BiobotPaneId::LevArena(levnum), PaneConclusion::ArenaDie) => BiobotPaneId::LevRetry(levnum),
-            (BiobotPaneId::LevRetry(levnum), PaneConclusion::SplashNext) => BiobotPaneId::LevArena(levnum),
             // TODO: Get max levnum from list of levels?
-            (BiobotPaneId::LevOutro(2), PaneConclusion::SplashNext) => BiobotPaneId::Win,
-            (BiobotPaneId::LevOutro(levnum), PaneConclusion::SplashNext) => BiobotPaneId::LevOutro(levnum+1),
-            (BiobotPaneId::Win, PaneConclusion::SplashNext) => BiobotPaneId::NewGame,
+            (BiobotPaneId::LevSplit(1), PaneConclusion::ArenaWin) => BiobotPaneId::Win,
+            (BiobotPaneId::LevSplit(levnum), PaneConclusion::ArenaWin) => BiobotPaneId::LevSplit(levnum+1),
+            (BiobotPaneId::Win, PaneConclusion::SplashNext) => Self::new().current_paneid,
             _ => panic!()
         };
     }
 
     pub fn load_pane(&self) -> Pane<super::ProgpuzzMovementLogic> {
         let aquarium1_key = HashMap::from([
-            // TODO: Combine with obj.char types?
+            // NB: Could it be combined with obj.char types?
             (' ', vec![ new_floor() ]),
             ('#', vec![ new_floor(), new_wall() ]),
             ('^', vec![ new_floor(), new_progbot() ]),
@@ -49,19 +40,9 @@ impl ProgpuzzLevset {
             */
         ]);
 
+        // NB: Would like to implement thin walls between squares, not walls filling whole squares.
         match self.current_paneid {
-            // TODO: Can we use idx++ instead of specifying each level number? Not immediately?
-            BiobotPaneId::NewGame => Pane::from_splash_dialogue(
-                //"Click or press [enter] to start.".to_string(),
-                vec![
-                    "Welcome to programming bot game!",
-                ]
-            ),
-
-            BiobotPaneId::LevIntro(1) => {
-                Pane::from_splash_string("Welcome to level 1!".to_string())
-            },
-            BiobotPaneId::LevArena(1) => Pane::<super::ProgpuzzMovementLogic>::from_play_ascii_map(&[
+            BiobotPaneId::LevSplit(1) => Pane::<super::ProgpuzzMovementLogic>::from_play_ascii_map(&[
                 "################",
                 "#              #",
                 "#              #",
@@ -79,20 +60,11 @@ impl ProgpuzzLevset {
                 "#              #",
                 "################",
             ], aquarium1_key),
-            BiobotPaneId::LevOutro(1) => {
-                Pane::from_splash_string("Well done!! Goodbye from level 1".to_string())
-            },
-
-            BiobotPaneId::LevRetry(_levno) => {
-                Pane::from_splash_string("Press [enter] to restart.".to_string())
-            },
             BiobotPaneId::Win => {
                 Pane::from_splash_string("Congratulations. You've completed all the levels. Press [enter] to play through again".to_string())
             },
 
-            BiobotPaneId::LevIntro(_) => panic!("Loading LevIntro for level that can't be found."),
-            BiobotPaneId::LevArena(_) => panic!("Loading LevArena for level that can't be found."),
-            BiobotPaneId::LevOutro(_) => panic!("Loading LevOutro for level that can't be found."),
+            BiobotPaneId::LevSplit(_) => panic!("Loading LevSplit for level that can't be found."),
         }
     }
 }
