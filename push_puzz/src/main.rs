4531db135f6ff6dad90cea2def3468c83a3b0756
Jack Vickeridge <jackv@cantab.net>
2025-11-08 17:15:57 +0000

    And

--- push_puzz/src/main.rs
+++ push_puzz/src/main.rs
@@ -1,11 +1,6 @@
 // Breadcrumb: Avoid warnings of unused code in each separare binary.
 // Try using lib crate + two binary crates in a workspace?
 
-#![feature(coroutines, coroutine_trait, iter_from_coroutine)]
-#![feature(try_trait_v2)]
-
-#![allow(unused_braces)]
-
 mod pushpuzz;
 mod engine_tests;
 
