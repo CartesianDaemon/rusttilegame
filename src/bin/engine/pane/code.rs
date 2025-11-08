b18c86eaa79ac6847c053fd9f9c2041fb8bf4f4f
Jack Vickeridge <jackv@cantab.net>
2025-11-08 05:16:48 +0000

    WIPW: Add Code pane with an outline program.

--- src/bin/engine/pane/code.rs
+++ src/bin/engine/pane/code.rs
@@ -2,9 +2,35 @@ use super::{PaneContinuation, PaneConclusion};
 use crate::engine::input::Input;
 use super::BasePane;
 
+// Todo: Could introduce AttemptAction with things like "move 1" and
+// "Rotate L/R", returned by a fn on Instr variants and similarly by
+// keys in pushpuzz. And interpreted further by an attempt_action fn
+// in simple_custom_props which examines passability etc.
+#[derive(Clone, Debug)]
+enum Instr {
+    F,
+    L,
+    R,
+    Loop(Vec<Instr>),
+}
+
+#[derive(Clone, Debug)]
+struct Bin<T> {
+    elem: T,
+    orig_count: u16,
+    curr_count: u16,
+}
+
+#[derive(Clone, Debug)]
+struct Flowchart<T> {
+    elems: Vec<T>,
+}
+
 #[derive(Clone, Debug)]
 pub struct Code {
     // Palette of available instructions, array of assembled instructions, etc.
+    supply: Vec<Bin<Instr>>,
+    prog: Flowchart<Instr>,
 }
 
 impl BasePane for Code
