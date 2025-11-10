1962ae4f1145cacc1faaedaa28debe92e04ce61a
Jack Vickeridge <jackv@cantab.net>
2025-11-10 23:08:25 +0000

    Tweak serve_locally

--- build_scripts/serve_locally.sh
+++ build_scripts/serve_locally.sh
@@ -7,6 +7,6 @@ cargo build --target wasm32-unknown-unknown # build debug wasm
 # Need to switch to chrome without opening new file
 # Or open file in new chrome eg. /mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe http://localhost:4000
 # explorer.exe # "http://localhost:4000"
-/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe http://localhost:4000
+/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe http://localhost:4000/programming_debug.html
 
 basic-http-server test_wasm/
