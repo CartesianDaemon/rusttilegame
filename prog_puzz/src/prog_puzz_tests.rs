d019f55836461ea49a4806f416d59f21600a886f
Jack Vickeridge <jackv@cantab.net>
2025-11-19 11:44:17 +0000

    Update prog_puzz tests.

--- prog_puzz/src/prog_puzz_tests.rs
+++ prog_puzz/src/prog_puzz_tests.rs
@@ -1,14 +1,11 @@
 use std::collections::HashMap;
 
-use macroquad::prelude::*;
-
 use tile_engine::for_gamedata::*;
 use super::objs::*;
 
-fn get_lev(n: i32) -> Widget<super::game_logic::ProgpuzzGameLogic> {
-    // NB: Use progpuzz key directly``
+fn basic_test_key() -> HashMap<char, Vec<FreeObj<crate::game_logic::ProgpuzzCustomProps>>> {
     let prog = Prog::from("F,F,R,F");
-    let test_key = HashMap::from([
+    HashMap::from([
         (' ', vec![ new_floor() ]),
         ('#', vec![ new_floor(), new_wall() ]),
         ('^', vec![ new_floor(), new_progbot_with_prog(CoordDelta::from_xy(0, -1), &prog) ]),
@@ -16,32 +13,32 @@ fn get_lev(n: i32) -> Widget<super::game_logic::ProgpuzzGameLogic> {
         ('v', vec![ new_floor(), new_progbot_with_prog(CoordDelta::from_xy(0, 1), &prog) ]),
         ('<', vec![ new_floor(), new_progbot_with_prog(CoordDelta::from_xy(-1, 0), &prog) ]),
         ('w', vec![ new_door_win() ]),
-    ]);
+    ])
+}
 
-    match n {
-        1 => Widget::CodingArena(CodingArena::new::<16>(
-            Arena::from_ascii(&[
-            "##############",
-            "#            #",
-            "#      w     #",
-            "#            #",
-            "#   ^        #",
-            "#            #",
-            "##############",
-            ], test_key),
-            Coding::from_ascii(
-                // NB: Consider crate macro to initialise vec
-                // NB: Consider my iteration macro here and elsewhere I collect'ed.
-                [
-                    ("F", 1),
-                    ("L", 1),
-                    ("R", 1),
-                    ("x2", 1),
-                ].into_iter().collect()
-            )
-        )),
-        _ => panic!(),
-    }
+fn get_basic_lev() -> Widget<super::game_logic::ProgpuzzGameLogic> {
+    // NB: Use progpuzz key directly``
+    Widget::CodingArena(CodingArena::new::<16>(
+        Arena::from_ascii(&[
+        "##############",
+        "#            #",
+        "#      w     #",
+        "#            #",
+        "#   ^        #",
+        "#            #",
+        "##############",
+        ], basic_test_key()),
+        Coding::from_ascii(
+            // NB: Consider crate macro to initialise vec
+            // NB: Consider my iteration macro here and elsewhere I collect'ed.
+            [
+                ("F", 1),
+                ("L", 1),
+                ("R", 1),
+                ("x2", 1),
+            ].into_iter().collect()
+        )
+    ))
 }
 
 #[test]
@@ -52,9 +49,39 @@ fn basic_move() {
     // Need to have decent visualisation for Pane::Something(Something).
     // Including checking that we move through level transitions ok.
 
-    let mut state = get_lev(1);
+    let mut state = get_basic_lev();
     // assert!(matches!(state, Pane::Split(phase: SplitPhase::Running, ..)));
-    assert_eq!(state.as_ascii_rows(), get_lev(1).as_ascii_rows());
+    assert_eq!(state.as_ascii_rows(), get_basic_lev().as_ascii_rows());
+
+    // Set up program that "user" has entered in code pane, that bot will follow.
+    if let Widget::CodingArena(split)= &mut state {
+        split.coding.prog =  Prog::from("F,F,R,F");
+    }
+
+    // Start running, no other effect
+    assert_eq!(state.advance(MoveCmd::Stay), PaneContinuation::Continue(()));
+    //assert_eq!(state.phase, SplitPhase::Coding);
+    assert_eq!(&state.as_ascii_rows()[4], "#   ^        #", "\n{}", state.as_ascii_rows().join("\n"));
+
+    assert_eq!(state.advance(MoveCmd::Stay), PaneContinuation::Continue(()));
+    //assert_eq!(state.phase, SplitPhase::Running);
+    assert_eq!(&state.as_ascii_rows()[3], "#   ^        #", "\n{}", state.as_ascii_rows().join("\n")); // F
+
+    assert_eq!(state.advance(MoveCmd::Stay), PaneContinuation::Continue(()));
+    assert_eq!(&state.as_ascii_rows()[2], "#   ^  w     #", "\n{}", state.as_ascii_rows().join("\n")); // F
+
+    assert_eq!(state.advance(MoveCmd::Stay), PaneContinuation::Continue(()));
+    assert_eq!(&state.as_ascii_rows()[2], "#   >  w     #", "\n{}", state.as_ascii_rows().join("\n")); // R
+
+    assert_eq!(state.advance(MoveCmd::Stay), PaneContinuation::Continue(()));
+    assert_eq!(&state.as_ascii_rows()[2], "#    > w     #", "\n{}", state.as_ascii_rows().join("\n")); // F
+}
+
+#[test]
+fn basic_move2() {
+    let mut state = get_basic_lev();
+    // assert!(matches!(state, Pane::Split(phase: SplitPhase::Running, ..)));
+    assert_eq!(state.as_ascii_rows(), get_basic_lev().as_ascii_rows());
 
     // Set up program that "user" has entered in code pane, that bot will follow.
     if let Widget::CodingArena(split)= &mut state {
