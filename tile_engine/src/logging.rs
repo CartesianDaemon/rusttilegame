e217a5d36cfac22dfa554023f9d6384c581deb1d
Jack Vickeridge <jackv@cantab.net>
2025-11-19 14:03:07 +0000

    Experiment with log format.
    No easy way to pass my additions to env_logger default formatter.
    - Could try to wrap logger in another logger?
    - Could copy formatting code?
    - Could wrap logging macros in my own version?
    
    Try using tracing crate, not logging. Has what I wanted in spans.

--- tile_engine/src/logging.rs
+++ tile_engine/src/logging.rs
@@ -1,13 +1,14 @@
-use std::io::Write;
-
 pub fn log_builder() -> env_logger::Builder {
     let mut log_builder = env_logger::Builder::new();
     log_builder.format_timestamp(None);
     log_builder.format_target(false);
-    // log_builder.format(|buf, record| {
-    //         let warn_style = buf.default_level_style(log::Level::Warn);
-    //         writeln!(buf, "{} - {warn_style}{}{warn_style:#}", record.level(), record.args())
-    //    });
+    if false {
+        log_builder.format(|buf, record| {
+                let warn_style = buf.default_level_style(log::Level::Warn);
+                use std::io::Write;
+                writeln!(buf, "{} - {warn_style}{}{warn_style:#}", record.level(), record.args())
+        });
+    }
     log_builder
 }
 
