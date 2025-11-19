ca4e66a2f2c3bb66bdf160e1cd35eaef9c9dcf92
Jack Vickeridge <jackv@cantab.net>
2025-11-19 13:46:19 +0000

    WIP: Tweaking log format.

--- tile_engine/src/logging.rs
+++ tile_engine/src/logging.rs
@@ -1,7 +1,13 @@
+use std::io::Write;
+
 pub fn log_builder() -> env_logger::Builder {
     let mut log_builder = env_logger::Builder::new();
     log_builder.format_timestamp(None);
     log_builder.format_target(false);
+    // log_builder.format(|buf, record| {
+    //         let warn_style = buf.default_level_style(log::Level::Warn);
+    //         writeln!(buf, "{} - {warn_style}{}{warn_style:#}", record.level(), record.args())
+    //    });
     log_builder
 }
 
