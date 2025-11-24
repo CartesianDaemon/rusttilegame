389896ee6152f337fa321240e4ed4f81941bcb80
Jack Vickeridge <jackv@cantab.net>
2025-11-24 21:54:10 +0000

    Use Node[] in tests.

--- tile_engine/src/widget/coding.rs
+++ tile_engine/src/widget/coding.rs
@@ -357,31 +357,31 @@ mod tests {
     #[test]
     fn test_simple_repeat() {
         let mut prog = Prog::from(vec![L, x2, L]);
-        prog.instrs[1].subnodes = Some(Prog::from(vec![F, R]));
+        prog[1].subnodes = Some(Prog::from(vec![F, R]));
         run_prog_and_test(prog, &[L,F,R,F,R,L]);
     }
 
     #[test]
     fn test_bare_repeat() {
         let mut prog = Prog::from(vec![x2]);
-        prog.instrs[0].subnodes = Some(Prog::from(vec![F]));
+        prog[0].subnodes = Some(Prog::from(vec![F]));
         run_prog_and_test(prog, &[F, F]);
     }
 
     #[test]
     fn test_bare_nested_repeat() {
         let mut prog = Prog::from(vec![x2]);
-        prog.instrs[0].subnodes = Some(Prog::from(vec![x2]));
-        prog.instrs[0].subnodes.as_mut().unwrap().instrs[0].subnodes = Some(Prog::from(vec![F]));
+        prog[0].subnodes = Some(Prog::from(vec![x2]));
+        prog[0][0].subnodes = Some(Prog::from(vec![F]));
         run_prog_and_test(prog, &[F, F, F, F]);
     }
 
     #[test]
     fn test_twice_nested_repeat() {
         let mut prog = Prog::from(vec![x2]);
-        prog.instrs[0].subnodes = Some(Prog::from(vec![x2, x2]));
-        prog.instrs[0].subnodes.as_mut().unwrap().instrs[0].subnodes = Some(Prog::from(vec![F]));
-        prog.instrs[0].subnodes.as_mut().unwrap().instrs[1].subnodes = Some(Prog::from(vec![R]));
+        prog[0].subnodes = Some(Prog::from(vec![x2, x2]));
+        prog[0][0].subnodes = Some(Prog::from(vec![F]));
+        prog[0][1].subnodes = Some(Prog::from(vec![R]));
         run_prog_and_test(prog, &[F, F, R, R, F, F, R, R]);
     }
 }
