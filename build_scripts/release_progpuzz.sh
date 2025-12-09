7808ddfb71860ff6d53300f88a52c20dbcba27d9
Jack Vickeridge <jackv@cantab.net>
2025-12-09 21:36:04 +0000

    Add .js files for saving levels in wasm. Works ok!

--- build_scripts/release_progpuzz.sh
+++ build_scripts/release_progpuzz.sh
@@ -5,3 +5,4 @@ cargo build -r --target wasm32-unknown-unknown --bin prog_puzz
 cp target/wasm32-unknown-unknown/release/prog_puzz.wasm docs/prog_puzz.wasm
 # Freeze version of assets used. TODO: Use rsync to remove old assets from docs/imgs.
 cp imgs/* docs/imgs/
+cp test_wasm/*.js docs/
